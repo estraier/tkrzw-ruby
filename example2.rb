8cf7901d0ab327e9077f60551a947507c737c7be
Mikio Hirabayashi <hirarin@gmail.com>
2021-07-24 17:52:29 +0900

    Add EOF tags.

--- example2.rb
+++ example2.rb
@@ -1,3 +1,18 @@
+#! /usr/bin/ruby -w
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Example for basic usage of the tree database
+#
+# Copyright 2020 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License.  You may obtain a copy of the License at
+#     https://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied.  See the License for the specific language governing permissions
+# and limitations under the License.
+#--------------------------------------------------------------------------------------------------
+
 require 'tkrzw'
 
 dbm = Tkrzw::DBM.new
@@ -64,3 +79,5 @@ ensure
   # Releases the resources.
   dbm.destruct
 end
+
+# END OF FILE
